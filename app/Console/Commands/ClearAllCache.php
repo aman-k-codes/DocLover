284e8bd6de59ca0ed7475d5565e6d2372cd6fad5
Aman Kumar <aman.kumar@iskconbangalore.org>
2025-03-28 17:33:35 +0530

    Version 1.0.0.8

---
+++ app/Console/Commands/ClearAllCache.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class ClearAllCache extends Command
+{
+    protected $signature = 'clear:all';
+    protected $description = 'Clear application, config, route, view, and compiled cache';
+
+    public function handle()
+    {
+        $this->call('cache:clear');
+        $this->call('config:clear');
+        $this->call('route:clear');
+        $this->call('view:clear');
+        $this->call('clear-compiled');
+
+        $this->info('All caches cleared successfully.');
+    }
+}
