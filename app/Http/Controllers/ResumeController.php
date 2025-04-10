d337c6ce291ac10c5c21e7af5d0f73472ce365f8
Aman Kumar <aman.kumar@iskconbangalore.org>
2025-04-10 15:23:51 +0530

    Version 1.0.2.5

--- app/Http/Controllers/ResumeController.php
+++ app/Http/Controllers/ResumeController.php
@@ -8,11 +8,11 @@ class ResumeController extends Controller
 {
     public function index()
     {
-        return view('sw.resumemaker.resume-home');
+        return view('resumemaker.resume-home');
     }
 
     public function ResumeTemplate()
     {
-        return view('sw.resumemaker.resume-template');
+        return view('resumemaker.resume-template');
     }
 }
