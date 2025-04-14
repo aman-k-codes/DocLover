d888e9fc10f98e0932dd9b2e6a613dde45a253cd
Aman Kumar <aman.kumar@iskconbangalore.org>
2025-04-14 12:18:51 +0530

    Version 1.0.2.6

--- resources/views/sw/pages/imageanddocumentprocessing/crop.blade.php
+++ resources/views/sw/pages/imageanddocumentprocessing/crop.blade.php
@@ -72,8 +72,10 @@
     </div>
 </section>
 
-<link href="https://unpkg.com/cropperjs/dist/cropper.min.css" rel="stylesheet" />
-<script src="https://unpkg.com/cropperjs/dist/cropper.min.js"></script>
+<!-- Updated (working) -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.13/cropper.min.css" rel="stylesheet" />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.13/cropper.min.js"></script>
+
 
 <script>
     let cropper;
