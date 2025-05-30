a056bf1e6197f37d869377d7dbd27312c2498ed6
Aman Sahu <amansahu2333924@gmail.com>
2025-05-30 21:39:40 +0530

    Version 1.0.6.3

--- resources/views/ads/ad1.blade.php
+++ resources/views/ads/ad1.blade.php
@@ -11,5 +11,13 @@
     <script type="text/javascript" src="//www.highperformanceformat.com/5ad69d9a945213bdf4131b95984b6985/invoke.js">
     </script> --}}
 
-    <h1>Ads Section 1</h1>
+    {{-- <h1>Ads Section 1</h1> --}}
+
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1623922509742047"
+        crossorigin="anonymous"></script>
+    <ins class="adsbygoogle" style="display:block" data-ad-format="autorelaxed" data-ad-client="ca-pub-1623922509742047"
+        data-ad-slot="5873834665"></ins>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
 </section>
--- resources/views/ads/ad2.blade.php
+++ resources/views/ads/ad2.blade.php
@@ -22,5 +22,13 @@
     <script type="text/javascript" src="//www.highperformanceformat.com/fdc01be9ec850783cb7c5be106ccdaf8/invoke.js">
     </script> --}}
 
-    <h1>Ads Section 2</h1>
+    {{-- <h1>Ads Section 2</h1> --}}
+
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1623922509742047"
+        crossorigin="anonymous"></script>
+    <ins class="adsbygoogle" style="display:block" data-ad-format="autorelaxed" data-ad-client="ca-pub-1623922509742047"
+        data-ad-slot="5873834665"></ins>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
 </section>
